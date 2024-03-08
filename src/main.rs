ac73618f0f094e7dce8bec5909ee5f03cdd3179b
Eugene <eugene97@connect.hku.hk>
2024-03-08 21:27:50 +0900

    test: create integration test module

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ use debt_tracer::telemetry::{get_subscriber, init_subscriber};
 async fn main() -> Result<(), std::io::Error> {
     let subscriber = get_subscriber("debt-tracer".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
-    run().await
+    run()?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,18 +1,20 @@
 use crate::configuration::get_configuration;
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub async fn run() -> Result<(), std::io::Error> {
+pub fn run() -> Result<Server, std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration");
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .bind(address)?
-        .run()
-        .await
+        .run();
+
+    Ok(server)
 }
