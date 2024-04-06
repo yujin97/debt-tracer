505a5c0552438e289dfc30637b963d568e5e86cf
Eugene <eugene97@connect.hku.hk>
2024-04-06 23:34:39 +0900

    test: setup tracing for integration tests

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,8 +1,23 @@
 use debt_tracer::configuration::get_configuration;
 use debt_tracer::configuration::DatabaseSettings;
 use debt_tracer::startup::get_connection_pool;
+use debt_tracer::telemetry::{get_subscriber, init_subscriber};
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 
 pub struct TestApp {
     pub address: String,
@@ -45,6 +60,8 @@ impl TestUser {
 }
 
 pub async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration.");
         c.database.database_name = Uuid::new_v4().to_string();
