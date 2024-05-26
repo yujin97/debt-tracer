99c31672e4088c18df5803119b95201ef40560d4
Eugene <eugene97@connect.hku.hk>
2024-05-26 21:31:42 +0900

    feat: add Session Middleware

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,11 @@
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::debt::{create_debt, get_debts_by_user_id};
+use actix_session::storage::RedisSessionStore;
+use actix_session::SessionMiddleware;
+use actix_web::cookie::Key;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use secrecy::{ExposeSecret, Secret};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -27,7 +31,13 @@ impl Application {
         let listener = TcpListener::bind(address)?;
         let port = listener.local_addr().unwrap().port();
 
-        let server = run(listener, db_pool)?;
+        let server = run(
+            listener,
+            db_pool,
+            configuration.application.hmac_secret,
+            configuration.redis_uri,
+        )
+        .await?;
 
         Ok(Self { port, server })
     }
@@ -41,10 +51,21 @@ impl Application {
     }
 }
 
-pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+pub async fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+    hmac_secret: Secret<String>,
+    redis_uri: Secret<String>,
+) -> Result<Server, anyhow::Error> {
     let db_pool = web::Data::new(db_pool);
+    let secret_key = Key::from(hmac_secret.expose_secret().as_bytes());
+    let redis_store = RedisSessionStore::new(redis_uri.expose_secret()).await?;
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(SessionMiddleware::new(
+                redis_store.clone(),
+                secret_key.clone(),
+            ))
             .route("/health_check", web::get().to(health_check))
             .route("/debts", web::get().to(get_debts_by_user_id))
             .route("/debt", web::post().to(create_debt))
