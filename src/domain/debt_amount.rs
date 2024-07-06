487c9508259b27608b47b4cfa2ad2bc86545df7a
Eugene <eugene97@connect.hku.hk>
2024-07-06 19:56:46 +0900

    feat: add details to DebtAmount and DebtCurrency

--- src/domain/debt_amount.rs
+++ src/domain/debt_amount.rs
@@ -1,1 +1,41 @@
+#[derive(Debug)]
 pub struct DebtAmount(f64);
+
+impl DebtAmount {
+    pub fn parse(amount: f64) -> Result<Self, String> {
+        let is_amount_negative = amount < 0.0;
+
+        let is_amount_not_a_number = amount.is_nan();
+
+        if is_amount_negative || is_amount_not_a_number {
+            return Err(format!("{} is not a positive number.", amount));
+        }
+
+        Ok(Self(amount))
+    }
+}
+
+impl AsRef<f64> for DebtAmount {
+    fn as_ref(&self) -> &f64 {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn negative_number_is_rejected() {
+        let amount = -69.0;
+
+        assert!(DebtAmount::parse(amount).is_err());
+    }
+
+    #[test]
+    fn positive_number_is_parsed_successfully() {
+        let amount = 420.69;
+
+        assert!(DebtAmount::parse(amount).is_ok());
+    }
+}
--- src/domain/debt_currency.rs
+++ src/domain/debt_currency.rs
@@ -1,1 +1,93 @@
-pub struct DebtCurrency(String);
+pub enum DebtCurrency {
+    AUD,
+    CAD,
+    CHF,
+    CNY,
+    EUR,
+    GBP,
+    HKD,
+    JPY,
+    KRW,
+    NZD,
+    SEK,
+    SGD,
+    USD,
+}
+
+const AUD_STR: &'static str = "AUD";
+const CAD_STR: &'static str = "CAD";
+const CHF_STR: &'static str = "CHF";
+const CNY_STR: &'static str = "CNY";
+const EUR_STR: &'static str = "EUR";
+const GBP_STR: &'static str = "GBP";
+const HKD_STR: &'static str = "HKD";
+const JPY_STR: &'static str = "JPY";
+const KRW_STR: &'static str = "KRW";
+const NZD_STR: &'static str = "NZD";
+const SEK_STR: &'static str = "SEK";
+const SGD_STR: &'static str = "SGD";
+const USD_STR: &'static str = "USD";
+
+impl std::fmt::Display for DebtCurrency {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use self::DebtCurrency::{AUD, CAD, CHF, CNY, EUR, GBP, HKD, JPY, KRW, NZD, SEK, SGD, USD};
+        match self {
+            AUD => AUD_STR.to_string().fmt(f),
+            CAD => CAD_STR.to_string().fmt(f),
+            CHF => CHF_STR.to_string().fmt(f),
+            CNY => CNY_STR.to_string().fmt(f),
+            EUR => EUR_STR.to_string().fmt(f),
+            GBP => GBP_STR.to_string().fmt(f),
+            HKD => HKD_STR.to_string().fmt(f),
+            JPY => JPY_STR.to_string().fmt(f),
+            KRW => KRW_STR.to_string().fmt(f),
+            NZD => NZD_STR.to_string().fmt(f),
+            SEK => SEK_STR.to_string().fmt(f),
+            SGD => SGD_STR.to_string().fmt(f),
+            USD => USD_STR.to_string().fmt(f),
+        }
+    }
+}
+
+impl DebtCurrency {
+    pub fn parse(s: String) -> Result<Self, String> {
+        use self::DebtCurrency::{AUD, CAD, CHF, CNY, EUR, GBP, HKD, JPY, KRW, NZD, SEK, SGD, USD};
+
+        // case insensitive
+        match s.to_uppercase().as_str() {
+            AUD_STR => Ok(AUD),
+            CAD_STR => Ok(CAD),
+            CHF_STR => Ok(CHF),
+            CNY_STR => Ok(CNY),
+            EUR_STR => Ok(EUR),
+            GBP_STR => Ok(GBP),
+            HKD_STR => Ok(HKD),
+            JPY_STR => Ok(JPY),
+            KRW_STR => Ok(KRW),
+            NZD_STR => Ok(NZD),
+            SEK_STR => Ok(SEK),
+            SGD_STR => Ok(SGD),
+            USD_STR => Ok(USD),
+            _ => Err(format!("{} is not a valid currency", s)),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn unknown_currency_is_rejected() {
+        let currency = "deez".to_string();
+
+        assert!(DebtCurrency::parse(currency).is_err());
+    }
+
+    #[test]
+    fn usd_is_parsed_successfully() {
+        let currency = USD_STR.to_string();
+
+        assert!(DebtCurrency::parse(currency).is_ok());
+    }
+}
